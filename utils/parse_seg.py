d79d62e8df063da879374738a6c5a223a76093f9
cilsat <cil.satriawan@gmail.com>
2017-07-31 05:48:32 +0700

    separated wav concatenation to write_wav function

--- utils/parse_seg.py
+++ utils/parse_seg.py
@@ -30,7 +30,7 @@ def lbl2df(path, start=10, filemap=False):
     cls = start
     for n, i in enumerate(lbls):
         df = pd.read_csv(i, delimiter=' ', index_col=0)
-        df['file'] = [n]*len(df) if filemap else [i.replace('lbl', 'mp3')]*len(df)
+        df['src'] = [n]*len(df) if filemap else [i.replace('lbl', 'mp3')]*len(df)
         cmap = {n: i + cls for i, n in enumerate(df.loc[df.lbl > 0, 'lbl'].unique())}
         for n in range(-2, 1): cmap[n] = n
         df['cls'] = df.lbl.map(cmap)
@@ -47,12 +47,12 @@ def lbl2df(path, start=10, filemap=False):
 def cplay(df):
     if type(df) != pd.core.series.Series:
         for _, i in df.iterrows():
-            print(i.name, i.cls, i.file, i.dur)
-            run(['play', i.file, 'trim', str(i.start*160)+'s', str(i.dur*160)+'s'] + dsp,
+            print(i.name, i.cls, i.src, i.dur)
+            run(['play', i.src, 'trim', str(i.start*160)+'s', str(i.dur*160)+'s'] + dsp,
                     stdin=PIPE, stdout=DEVNULL, stderr=DEVNULL, start_new_session=True)
     else:
-        print(df.name, df.cls, df.file, df.dur*160)
-        run(['play', df.file, 'trim', str(df.start*160)+'s', str(df.dur*160)+'s'] + dsp,
+        print(df.name, df.cls, df.src, df.dur*160)
+        run(['play', df.src, 'trim', str(df.start*160)+'s', str(df.dur*160)+'s'] + dsp,
                 stdin=PIPE, stdout=DEVNULL, stderr=DEVNULL, start_new_session=True)
 
 
@@ -68,13 +68,14 @@ def splay(seg, df, *args):
         play(seg, df.loc[df.spkr == spk])
 
 
-def lbl2seg(name, path=True, s='lbl'):
+def lbl2seg(name, path=False, s='cls'):
+    # name denotes path to src if path=False, dataframe otherwise
     if path:
         name = name.split('.')[0]
         df = pd.read_csv(path, delimiter=' ', index_col=0)
     else: df = name
 
-    df.index = df.file.str[:4] if 'file' in df.columns else [name]*len(df)
+    df.index = df.src.str.split('.')[0] if 'src' in df.columns else [name]*len(df)
     gmap = {-1: 'U', 0: 'M', 1: 'F'}
     df['gen'] = df['gen'].map(gmap)
     fill = len(str(df[s].max()))
@@ -125,28 +126,40 @@ def calc_der(lbl):
     return 1.0 - (cmat[ref, hyp].sum() / spk.dur.sum())
 
 
+# df must have at least start, dur, src, and dest columns
+def write_wav(df):
+    if len(df.src.unique()) > 1 or len(df.dest.unique()) > 1:
+        print('too many input/output files')
+        return
+
+    times = np.dstack((df.start.values, (df.start + df.dur).values))
+    trims = ['='+str(n)+'s' for n in times.flatten()*160]
+    run(['sox', df.src.iloc[0], df.dest.iloc[0]
+        'trim'] + trims + ['gain', '-6', 'highpass', '120'],
+            stdin=PIPE, stdout=DEVNULL, stderr=DEVNULL)
+
+
 def make_spk(dfs, path='/home/cilsat/data/speech/rapat', min_dur=12000):
+    spk = []
     for n in dfs.cls.unique():
-        dfc = dfs.loc[dfs.cls == cls]
+        dfc = dfs.loc[dfs.cls == n]
         cum = dfc.dur.cumsum()
-        dur = cum.max()
-        if cum.max() < min_dur: print('not enough data')
+        if cum.max() < min_dur: print('not enough data for ' + str(n))
         else:
             df = dfc.loc[:cum.loc[cum > min_dur].index[0]].copy()
             # get start and end of segments in samples
-            times = np.dstack((df.start.values, (df.start + df.dur).values))
-            trims = ['='+str(n)+'s' for n in times.flatten()*160]
-            # write segments to file
-            old = os.path.join(path, df.file.iloc[0])
-            new = 's'+str(cls).zfill(3)
-            run(['sox', old, os.path.join(path, 'spk/'+new+'.wav'),
-                'trim'] + trims + ['gain', '-6', 'highpass', '120'],
-                    stdin=PIPE, stdout=DEVNULL, stderr=DEVNULL)
-            df.file = new
             df.drop(['spkr', 'lbl'], axis=1, inplace=True)
             df.start = np.append([0], df.dur.cumsum()[:-1].values)
-            lbl2seg(df, path=False, s='cls').to_csv(os.path.join(path, 'spk/'+new+'.seg'), sep=' ', header=None)
-            return df
+            spk.append(df)
+
+    spk = pd.concat(spk)
+    old = spk.src.iloc[0]
+    spk['src'] = old
+    new = old.split('.')[0]+'_spk'+'_'+str(min_dur/100)+'s'
+    spk['dest'] = new
+
+    return spk
+    #lbl2seg(df, path=False, s='cls').to_csv(os.path.join(path, 'spk/'+new+'.seg'), sep=' ', header=None)
 
 
 def make_ubm(out, path='/home/cilsat/data/speech/rapat', min_dur=9000, min_spk=3, max_spkr=120):
@@ -155,7 +168,7 @@ def make_ubm(out, path='/home/cilsat/data/speech/rapat', min_dur=9000, min_spk=3
     # get all clusters that are not the first min_spk speakers in each file
     # the assumption here is that the first min_spk speakers are repeated across
     # meetings
-    spk = dfs.loc[dfs.groupby(dfs.file).apply(lambda x: x.loc[x.lbl.isin(x.lbl.unique()[min_spk:])]).index.get_level_values(1)]
+    spk = dfs.loc[dfs.groupby(dfs.src).apply(lambda x: x.loc[x.lbl.isin(x.lbl.unique()[min_spk:])]).index.get_level_values(1)]
     spk = spk.loc[spk.lbl > 1]
     # find clusters that have at least min_dur seconds of speech and get them
     segs = []
@@ -170,19 +183,19 @@ def make_ubm(out, path='/home/cilsat/data/speech/rapat', min_dur=9000, min_spk=3
             times = np.dstack((df.start.values, (df.start + df.dur).values))
             trims = ['='+str(n)+'s' for n in times.flatten()*160]
             # write segments to file
-            old = os.path.join(path, df.file.iloc[0])
+            old = os.path.join(path, df.src.iloc[0])
             new = 'c'+str(n).zfill(3)
             run(['sox', old, os.path.join(path, 'ubm/'+new+'.wav'), 'trim'] + trims,
                     stdin=PIPE, stdout=DEVNULL, stderr=DEVNULL)
-            df['bak'] = df.file.str[:-4]
-            df.file = new
+            df['src'] = df.src.str[:-4]
+            df['dest'] = new
             df.drop(['spkr', 'lbl'], axis=1, inplace=True)
             segs.append(df)
             count += 1
 
     segs = pd.concat(segs)
     segs.start = np.append([0], segs.dur.cumsum()[:-1].values)
-    infiles = [os.path.join(path, 'ubm/'+n+'.wav') for n in segs.file.unique()]
+    infiles = [os.path.join(path, 'ubm/'+n+'.wav') for n in segs.src.unique()]
     cmd = ['sox'] + infiles + [os.path.join(path, 'ubm/'+out+'.wav'), 'gain', '-6', 'highpass', '120']
     run(cmd, stdin=PIPE, stdout=DEVNULL, stderr=DEVNULL)
     name = os.path.join(path, 'ubm/' + out)
