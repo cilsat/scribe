4949dde30c7b096ca63a9a2a9a3e93313de79a3f
cilsat <cil.satriawan@gmail.com>
2017-10-01 22:49:36 +0700

    Added gst tests

--- utils/pygst_test.py
+++ utils/pygst_test.py
@@ -1,34 +1,36 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python
 
 import sys
+import json
 import gi
 gi.require_version('Gst', '1.0')
-
 from gi.repository import Gst, GLib
+from collections import OrderedDict
 
 Gst.init(None)
 
 
 class Cutter(object):
 
-    def __init__(self):
+    def __init__(self, config):
         self.loop = GLib.MainLoop()
 
-        self.src = Gst.ElementFactory.make('filesrc', 'filesrc')
-        self.wavparse = Gst.ElementFactory.make('wavparse', 'wavparse')
-        self.cutter = Gst.ElementFactory.make('cutter', 'cutter')
-        self.sink = Gst.ElementFactory.make('autoaudiosink', 'autoaudiosink')
-
-        self.src.set_property('location', sys.argv[1])
-        #self.cutter.set_property('threshold-dB', -5.0)
+        self.plugins = [Gst.ElementFactory.make(
+            plug, plug) for plug in config.keys()]
 
         self.pipeline = Gst.Pipeline()
-        for element in [self.src, self.wavparse, self.cutter, self.sink]:
-            self.pipeline.add(element)
+        for n, plug in enumerate(config.keys()):
+            options = config[plug]
+            if options:
+                for o in options.keys():
+                    try:
+                        self.plugins[n].set_property(o, config[plug][o])
+                    except Exception as e:
+                        print(e)
+            self.pipeline.add(self.plugins[n])
 
-        self.src.link(self.wavparse)
-        self.wavparse.link(self.cutter)
-        self.cutter.link(self.sink)
+        for n in range(len(config.keys()) - 1):
+            self.plugins[n].link(self.plugins[n + 1])
 
         bus = self.pipeline.get_bus()
         bus.add_signal_watch()
@@ -36,23 +38,20 @@ class Cutter(object):
         bus.connect('message::error', self.on_error)
         bus.connect('message::eos', self.on_eos)
 
-    def start(self):
+    def run(self):
         ret = self.pipeline.set_state(Gst.State.PLAYING)
         if ret == Gst.StateChangeReturn.FAILURE:
-            print("ERROR")
+            print("Error opening pipeline")
             sys.exit(1)
 
         self.loop.run()
 
-        self.pipeline.set_state(Gst.State.NULL)
-
     def on_cut(self, bus, msg):
-        print(msg.get_structure().get_name())
-        #s = msg.structure
-        #lvl = 'below'
-        # if s['above']:
-        #    lbl = 'above'
-        #print(Gst.TIME_ARGS(s['timestamp']), lvl)
+        s = msg.get_structure()
+        lvl = 'below'
+        if s['above']:
+            lbl = 'above'
+        print(Gst.TIME_ARGS(s['timestamp']), lvl)
 
     def on_error(self, bus, msg):
         err, dbg = msg.parse_error()
@@ -66,5 +65,8 @@ class Cutter(object):
 
 
 if __name__ == '__main__':
-    c = Cutter()
-    c.start()
+    with open(sys.argv[1]) as cfg:
+        config = json.load(cfg, object_pairs_hook=OrderedDict)
+
+    c = Cutter(config)
+    c.run()
